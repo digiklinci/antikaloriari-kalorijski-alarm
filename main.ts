ef39d7aaab9cc8f48af201467c3493894ac48002
digiklinci <165515192+digiklinci@users.noreply.github.com>
2024-04-12 07:54:32 +0200

    Ажурирање pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,25 +1,61 @@
-radio.onReceivedNumberDeprecated(function (receivedNumber) {
-    if (receivedNumber >= 10) {
-        basic.showIcon(IconNames.Heart)
-    } else {
-        basic.clearScreen()
-        for (let index = 0; index < 4; index++) {
+function otkljucaj (brojKalorija: number) {
+    aktiviran = 0
+    // vreme deaktiviranje je duze ako je broj utrosenih kalorija veci
+    for (let index = 0; index < brojKalorija / 10; index++) {
+        basic.pause(1000)
+    }
+    basic.showIcon(IconNames.Yes)
+}
+function alarm () {
+    if (aktiviran == 1) {
+        for (let index = 0; index < 10; index++) {
+            music.play(music.tonePlayable(880, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
             basic.pause(100)
-            music.play(music.tonePlayable(988, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+        }
+    }
+}
+// deaktiviranje
+input.onGesture(Gesture.Shake, function () {
+    basic.clearScreen()
+    kalorijskiPrag = 100
+    aktiviran = 0
+    basic.showIcon(IconNames.No)
+})
+// aktiviranje
+input.onButtonPressed(Button.B, function () {
+    aktiviran = 1
+    basic.showIcon(IconNames.Yes)
+})
+// prijemnik
+radio.onReceivedNumberDeprecated(function (primljeneKalorije) {
+    if (primljeneKalorije >= kalorijskiPrag) {
+        basic.showIcon(IconNames.Heart)
+        otkljucaj(primljeneKalorije)
+        // naplati antikalorimetru
+        radio.sendNumber(primljeneKalorije - kalorijskiPrag)
+    } else {
+        basic.showIcon(IconNames.Yes)
+        radio.sendNumber(0)
+    }
+})
+// menjanje praga kalorija
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    basic.clearScreen()
+    kalorijskiPrag += 20
+    basic.showNumber(kalorijskiPrag)
+})
+let ubrzanje = 0
+let aktiviran = 0
+let kalorijskiPrag = 0
+basic.showIcon(IconNames.Duck)
+radio.setGroup(0)
+kalorijskiPrag = 100
+basic.forever(function () {
+    if (aktiviran == 1) {
+        ubrzanje = input.acceleration(Dimension.Strength) - 1023
+        // okidač
+        if (ubrzanje > 10) {
+            alarm()
         }
     }
 })
-input.onButtonPressed(Button.A, function () {
-    basic.showIcon(IconNames.Yes)
-})
-input.onGesture(Gesture.Shake, function () {
-    for (let index = 0; index < 5; index++) {
-        basic.pause(100)
-        music.play(music.tonePlayable(784, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
-    }
-})
-input.onButtonPressed(Button.B, function () {
-    basic.clearScreen()
-})
-basic.showIcon(IconNames.Duck)
-radio.setGroup(0)
